acaec18b12edcbb1650fd1a8a34aebf3b9a5b879
yaozhuoyu <yaozhuoyu@gmail.com>
2014-08-28 12:42:13 +0800

    修改.gitignore,初始化工程

---
+++ iOS-Samples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS-Samples
+//
+//  Created by 姚卓禹 on 14-8-28.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS-SamplesTests/iOS_SamplesTests.m
@@ -0,0 +1,34 @@
+//
+//  iOS_SamplesTests.m
+//  iOS-SamplesTests
+//
+//  Created by 姚卓禹 on 14-8-28.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS_SamplesTests : XCTestCase
+
+@end
+
+@implementation iOS_SamplesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
